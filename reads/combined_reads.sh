a68be641d13bb32681f63f0d39b320e31c5f19ac
sophie <willetse0745@uwec.edu>
2025-12-03 11:58:31 -0600

    combined reads

--- reads/combined_reads.sh
+++ reads/combined_reads.sh
@@ -15,7 +15,7 @@ conda activate biol343
 # Set variables
 FC1="/data/groups/wheelenj/sequencing/20250916_M009242/4650P/Planaria1/20250909_1219_3B_PBE82635_fbdcc6f4/fastq_pass"
 FC2="/data/groups/wheelenj/sequencing/20250916_M009242/4651P/Planaria2/20250909_1354_3C_PBE69031_20f7cd5b/fastq_pass"
-OUTFILE="planaria_all_reads.fastq.gz"
+OUTFILE="/data/groups/wheelenj/sequencing/20250916_M009242/planaria_all_reads.fastq.gz"
 
 # Run concatenation
 echo "Starting concatenation at $(date)"
